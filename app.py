dc06a63671fd52cb2f5c453a2d286137bc91a82d
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-06-30 12:05:21 -0700

    Add form to submit a guess

---
+++ app.py
@@ -0,0 +1,27 @@
+from boggle import Boggle
+from flask import Flask, request, render_template, session,redirect
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_boggle"
+debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+boggle_game = Boggle()
+
+@app.route('/')
+def display_board():
+    """Display the Boggle board."""
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template('board.html', board=board)
+
+@app.route('/submit', methods=["POST"])
+def check_guess():
+    """Submit a guess without refreshing the screen."""
+    guess = request.args.get('guess')
+    # post = request.get_json()
+    print(guess)
+    return redirect('/')
+
+
---
+++ static/app.js
@@ -0,0 +1,53 @@
+const htmlBoard = document.querySelector('#board')
+const form = document.querySelector('form')
+const $inputData = $('input')
+const $guess = $inputData.val()
+
+//Creating HTML boggle board
+function makeHtmlBoard() {
+//creating squares for board
+    for (let y = 0; y < 5; y++) {
+        const tr = document.createElement('tr');
+        htmlBoard.append(tr)
+
+        for (let x = 0; x < 5; x++) {
+            const td = document.createElement('td');
+            td.setAttribute('id', `${y}-${x}`);
+            tr.append(td);
+        }
+    }
+}
+
+//Putting each character in squares
+function putCharInBoard() {
+    for (let y = 0; y < 5; y ++) {
+        for(let x = 0; x < 5; x++ ){
+            const sq = document.getElementById(`${y}-${x}`)
+            sq.innerText = board[y][x]
+        }
+    }
+}
+
+
+//Getting the guess that was submitted
+// function getInputData() {
+//     const guess = $inputData.val()
+//     console.log('input data', guess);
+// }
+
+//Sending data to the server
+async function sendInputData() {
+    const res = await axios.post('/submit', `${$inputData.val()}`);
+    console.log('guess', $inputData.val());
+    console.log('res', res);
+}
+
+
+console.log(board);
+makeHtmlBoard();
+putCharInBoard();
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    sendInputData();
+})
