325388d4f27adf2978ddb8ab508e93c733efd641
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-07-04 10:25:58 -0700

    implemetnting to send stats data after game ends

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, session,redirect
+from flask import Flask, request, redirect, render_template, session, jsonify, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -8,19 +8,56 @@ debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 boggle_game = Boggle()
+game_played = 0
+best_score = 0
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def display_board():
     """Display the Boggle board."""
     board = boggle_game.make_board()
+    global game_played, best_score
+    res = request.get_json()
+    print('exist?', session.get('game_played'))
+    print('bestScore received', res[best_score])
+    if session.get('game_played') == True:
+        game_played = session['game_played']
+    else:
+        session['game_played'] = game_played
+
+    if session.get('best_score') == True:
+        best_score = res[best_score]
+        best_score = session['best_score']
+    else:
+        session['best_score'] = best_score
+
     session['board'] = board
     return render_template('board.html', board=board)
 
-@app.route('/submit', methods=["POST"])
-def check_guess():
-    """Submit a guess without refreshing the screen."""
-    guess = request.args.get('guess')
-    print(guess)
-    return redirect('/')
+
+@app.route('/check-word', methods=['POST'])
+def check_word():
+    """Get post request data and check if it's valid."""
+    post = request.get_json()
+    word = post['word']
+    board = session["board"]
+    # game_played = post['gamePlayed']
+    # print('gameplayed', game_played)
+    response = boggle_game.check_valid_word(board, word)
+    return jsonify({'result': response})
 
 
+@app.route('/stats', methods=['POST'])
+def display_stats():
+    """Display game stats."""
+    post = request.get_json()
+    print('post', post)
+    global game_played
+    global best_score
+    if game_played >= 1:
+        game_played += 1
+    session['game_played'] = game_played
+    session['best_score'] = best_score
+    if post['score'] > best_score:
+        session['best_score'] = post['score']
+    print('gamePlayed', session['game_played'])
+    return jsonify({'gamePlayed': game_played, 'bestScore': best_score})
--- static/app.js
+++ static/app.js
@@ -1,11 +1,17 @@
 const htmlBoard = document.querySelector('#board')
 const form = document.querySelector('form')
 const $inputData = $('input')
-const $guess = $inputData.val()
+const $msgContainer = $('#msg-container')
+let score = 0;
+let gamePlayed;
+let bestScore;
+let timeleft = 60;
 
 //Creating HTML boggle board
 function makeHtmlBoard() {
-//creating squares for board
+    //diplaying game container
+    $('#game-container').show();
+    //creating squares for board
     for (let y = 0; y < 5; y++) {
         const tr = document.createElement('tr');
         htmlBoard.append(tr)
@@ -20,8 +26,8 @@ function makeHtmlBoard() {
 
 //Putting each character in squares
 function putCharInBoard() {
-    for (let y = 0; y < 5; y ++) {
-        for(let x = 0; x < 5; x++ ){
+    for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
             const sq = document.getElementById(`${y}-${x}`)
             sq.innerText = board[y][x]
         }
@@ -29,29 +35,171 @@ function putCharInBoard() {
 }
 
 
-//Getting the guess that was submitted
-// function getInputData() {
-//     const guess = $inputData.val()
-//     console.log('input data', guess);
-// }
+//Displaying different messages depending on the result.
+function displayResult(result) {
+    //if there's message displayed already, erase it.
+    if ($('#message')) {
+        $('#message').text('');
+    }
+    if (result == 'not-on-board') {
+        msg = "This word is not on the board."
+    } else if (result == 'not-word') {
+        msg = "This word doesn't exist."
+    } else if (result == 'ok') {
+        msg = "Great!"
+    }
+    $msgContainer.append($(`<p id="message"></p>`))
+    return $('#message').text(`${msg}`)
+}
 
-//Sending data to the server
-async function sendInputData() {
-    const res = await axios.post('/submit',{
-        data: $inputData.val()
-    })
-    .then((res) => {
-    console.log('guess', $inputData.val());
-    console.log('res', res);
-    });
+function handleScore(word) {
+    console.log('word length', word.length)
+    if (word.length >= 8) {
+        score += 11;
+    } else if (word.length >= 7) {
+        score += 5;
+    } else if (word.length >= 6) {
+        score += 3;
+    } else if (word.length >= 5) {
+        score += 2;
+    } else if (word.length >= 3) {
+        console.log('3letters')
+        console.log(score);
+        score += 1;
+    }
+    console.log(score);
+    if (score > bestScore){
+        bestScore = score;
+        console.log('updated bestScore!', bestScore)
+    }
+
+    if (document.querySelector('#score')) {
+        $('#score').text(`Your Current Score: ${score}`);
+    } else {
+        $('#score-container').append($(`<p id="score">Your Current Score: ${score}</p>`));
+    }
 }
 
 
-console.log(board);
+async function handleStats(){
+    if (document.querySelector('#best-score')){
+        $('#best-score').text(`Your Best Score: ${bestScore}`);
+    } else {
+        console.log('hit', bestScore)
+        $('#score-container').append($(`<p id="best-score">Your Best Score: ${bestScore}</p>`));
+    
+    }
+
+    if (document.querySelector('#game-played')){
+        $('#game-played').text(`Game Played: ${gamePlayed}`);
+    } else {
+        console.log('hit', gamePlayed)
+        $('#score-container').append($(`<p id="game_played">Game Played: ${gamePlayed}</p>`));
+    }
+}
+
+async function getData() {
+    customConfig = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    data = {
+        score: score,
+        game_played: gamePlayed,
+        best_score: bestScore
+    }
+    //first, get/send data to the server side
+    const res = await axios.post('/stats', data, customConfig);
+    console.log('data sent', res.data)
+    gamePlayed = res.data['gamePlayed']
+    bestScore = res.data['bestScore']
+    //this function is here in order to retrieve gamePlayed and bestScore first to display.
+    handleStats();
+}
+
+
+//Sending data to the server
+async function sendInputData() {
+    //couldn't use "resquest.get.data," so changed it to json
+    customConfig = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    const inputData = {
+        word: $inputData.val(),
+    }
+    const res = await axios.post('/check-word', inputData, customConfig);
+
+    //recieving result from check-word
+    console.log('from sendInputData()',res.data)
+    const result = res.data.result
+    // gamePlayed = res.data.gamePlayed
+    // console.log(gamePlayed);
+    displayResult(result)
+    if (result == 'ok') {
+        handleScore($inputData.val())
+    }
+    
+    
+}
+
+function displayTime() {
+    $msgContainer.append($('<p>Remaining time: <span id="time"></span> seconds</p>'));
+    timer
+}
+
+const timer = setInterval(function () {
+    if (timeleft <= 0) {
+        clearInterval(timer);
+        $('#message').text("Time's Up!");
+        $('#form-container').hide();
+        timeUp();
+    }
+    $('#time').text(`${timeleft}`);
+    timeleft--;
+}, 1000);
+
+//When time is up hide the submit button, display the Play Again button, and send data to the server side.
+function timeUp() {
+    $('#message').text("Time's Up!");
+    $('#form-container').hide();
+    $('#game-container').append($('<button id="play-again-btn">Play Again</button>'));
+    // gamePlayed += 1;
+    console.log('gamePlayed right before sending', gamePlayed);
+    $('#game-container').on('click', $('#play-again-btn'),function(){
+        console.log('clicked!')
+        sendStats();
+        location.reload()
+    });
+}
+
+//Send stats to the server at the end of the game
+async function sendStats(){
+    customConfig = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    const data = {
+        best_score: bestScore
+    }
+    const res = await axios.post('/', data, customConfig);
+
+    console.log('from getStats', res);
+}
+
+
 makeHtmlBoard();
 putCharInBoard();
+getData();
+displayTime();
+console.log(board);
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    sendInputData();
+form.addEventListener('submit', async function (e) {
+    e.preventDefault()
+        //empty the input field after sending data
+        await sendInputData();
+        $inputData.val('');
 })
